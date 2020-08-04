b1b3cf716f0fb256bf738de6eb9874e20e760210
MichaelCarrasco <67906994+MichaelCarrasco@users.noreply.github.com>
2020-08-04 12:52:41 -0400

    Create Cipher.java

---
+++ Assignment 1/src/Cipher.java
@@ -0,0 +1,51 @@
+/* Programming Fundamentals
+ * NAME: Michael Carrasco
+ * PROGRAMMING ASSIGNMENT 1
+ * Summer 2020
+ */
+
+import java.util.Scanner; 
+import java.util.Random;
+public class Cipher {
+
+	public static void main(String[] args) {
+		System.out.println("Programming Fundamentals");
+		System.out.println("NAME: Michael Carrasco");
+		System.out.println("PROGRAMMING ASSIGNMENT 1");
+		System.out.println("\nWelcome to the Cipher program");
+		
+		Random rand= new Random();
+		Scanner input = new Scanner(System.in);
+		
+		int sum = 0; //starts value of sum at 0 for arithmetic purposes
+		int []integer= new int[5]; //creates an array that can hold 5 values
+		System.out.println("Please enter 5 numbers between 0 and 19");
+		
+		for (int i=0; i<5; i++) {
+			integer[i] = input.nextInt(); //User input for integers
+			sum = sum + integer[i]; //sums integers into plainText(unencoded number)
+			if (integer[i] < 0 || integer[i] > 19 ) {
+				System.out.println("Please read directions and try again!");
+				System.exit(0);
+			}
+		}
+		System.out.println("1st number: " +integer[0]);
+		System.out.println("2nd number: " +integer[1]);
+		System.out.println("3rd number: " +integer[2]);
+		System.out.println("4th number: " +integer[3]);
+		System.out.println("5th number: " +integer[4]);
+		System.out.println("\nTotal = "+sum);
+		
+		int key=rand.nextInt(10); //Randomizes value from 0 to 9(inclusive)
+		System.out.println("Your random key is " + key);
+		
+		int firstDigit = sum/10; //calculates first digit of number
+		int secondDigit = sum%10; //calculates second digit of number
+		int encoded = (((firstDigit + key))%10)*10 + (((secondDigit + key))%10); //encodes the two digits together with the key
+		System.out.println("Your encoded number is " + encoded); 
+		input.close();
+		
+		
+	}
+
+}
